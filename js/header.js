3d159d42f674a22cb72f18caca38300811cdab71
cabit <bettec@usi.ch>
2023-02-08 23:27:37 +0100

    Aye, seems working

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.write(`
 
 <header>
     <h1>CARLO BETTELINI - PORTFOLIO</h1> 
-    <h5 style = "text-align: center;"> Videomaker | Junior Software Developer | .Net Framework | Unity | Adobe Premiere Pro | Vegas Pro</h5> 
+    <h5 style = "text-align: center; margin-bottom: 10px;"> Videomaker | Junior Software Developer | .Net Framework | Unity | Adobe Premiere Pro | Vegas Pro</h5> 
 </header>
 <div id="central">
         <div id="navigation">
@@ -15,9 +15,6 @@ document.write(`
                 </div>
                 <div class="page-link">
                     <a href="./videomaker.html">Videomaker</a>
-                </div>
-                <div class="page-link">
-                    <a href="./programs.html">Programs</a>
                 </div>
             </nav>
         </div>
