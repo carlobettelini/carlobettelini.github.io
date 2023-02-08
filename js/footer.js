0a641069b5c36f92fa59e08e72d5592dfe63fed8
cabit <bettec@usi.ch>
2023-02-08 18:29:46 +0100

    Other main changes

---
+++ js/footer.js
@@ -0,0 +1,21 @@
+document.write(`
+
+    <footer>
+        <p>© Carlo Bettelini, last page update 7.02.2023</p>
+        <div id="infos">
+            <div class="info"><img src="./images/logos/gmail.png " class="images-info ">
+                <p>carlo.bettelini@gmail.com</p>
+            </div>
+            <div class="info"><img src="./images/logos/phone.png " class="images-info ">
+                <p>+41 (0) 79 560 14 58</p>
+            </div>
+        </div>
+        <div id="media-container">
+            <a href="https://www.instagram.com" target="_blank"><img src="./images/logos/instagram.png" alt="Instagram contact" width="50" height="50"></a>
+            <a href="http://facebook.com/profile.php?=73322363" target="_blank"><img src="./images/logos/Facebook_logo.png" alt="Facebook" width="50" height="50"></a>
+            <a href="https://www.youtube.com/@TheTehnigga" target="_blank"><img src="./images/logos/youtube.png" alt="Youtube" width="50" height="50"></a>
+            <a href="https://www.imdb.com/name/nm7644480/" target="_blank"><img src="./images/logos/imdb.png" alt="IMD" width="50" height="50"></a>
+        </div>
+    </footer>
+
+`);
---
+++ js/header.js
@@ -0,0 +1,24 @@
+document.write(`
+
+<header>
+    <h2 style = "text-align: center;"> Portfolio </h2> 
+    <h1> Carlo Bettelini </h1> 
+    <h3 style = "text-align: center;" > Videomaker | Junior Software Developer | .Net Framework | Unity | Adobe Premiere Pro | Vegas Pro </h3> 
+</header >
+<div id="central">
+        <div id="navigation">
+            <nav>
+                <div class="current-page ">Homepage</div>
+                <div class="page-link ">
+                    <a href="./about.html ">About me</a>
+                </div>
+                <div class="page-link ">
+                    <a href="./media.html ">Media</a>
+                </div>
+                <div class="page-link ">
+                    <a href="./programs.html ">Programs</a>
+                </div>
+            </nav>
+        </div>
+
+`);
