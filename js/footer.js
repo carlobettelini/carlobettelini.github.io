c2141fddb9762d576a2d0cd54e69b8a4399679fa
cabit <bettec@usi.ch>
2023-02-09 19:12:33 +0100

    Pushing changes

--- js/footer.js
+++ js/footer.js
@@ -1,20 +1,39 @@
 document.write(`
-
+<div class="footer">
+<p style="padding: 10px;">© Carlo Bettelini, last page update 7.02.2023</p>
+<div id="infos">
+    <div class="info"><img src="./images/logos/gmail.png " class="images-info">
+        <p style="padding: 10px;">carlo.bettelini@gmail.com</p>
+    </div>
+    <div class="info"><img src="./images/logos/phone.png" class="images-info">
+        <p style="padding: 10px;">+41 (0) 79 560 14 58</p>
+    </div>
+</div>
+<div id="media-container">
+    <a style="margin-right: 10px;" href="https://www.instagram.com" target="_blank"><img style="max-width: 50px;" src="./images/logos/instagram.png" alt="Instagram contact"></a>
+    <a style="margin-right: 10px;" href="http://facebook.com/profile.php?=73322363" target="_blank"><img style="max-width: 50px;" src="./images/logos/Facebook_logo.png" alt="Facebook"></a>
+    <a style="margin-right: 10px;" href="https://www.youtube.com/@TheTehnigga" target="_blank"><img style="max-width: 50px;" src="./images/logos/youtube.png" alt="Youtube"></a>
+    <a style="margin-right: 10px;" href="https://www.imdb.com/name/nm7644480/" target="_blank"><img style="max-width: 50px;" src="./images/logos/imdb.png" alt="IMD"></a>
+</div>
+</div>
     <div id="footer">
-        <p style="padding: 10px;">© Carlo Bettelini, last page update 7.02.2023</p>
+        <p style="padding: 10px;">© Carlo Bettelini, last page update 9.02.2023</p>
         <div id="infos">
-            <div class="info"><img src="./images/logos/gmail.png " class="images-info">
-                <p style="padding: 10px;">carlo.bettelini@gmail.com</p>
-            </div>
+            <a href="mailto:"carlo.bettelini@gmail.com">
+                <div class="info">
+                    <img src="./images/logos/gmail.png " class="images-info">
+                    <p style="padding: 10px;">carlo.bettelini@gmail.com</p>
+                </div>
+            </a>
             <div class="info"><img src="./images/logos/phone.png" class="images-info">
                 <p style="padding: 10px;">+41 (0) 79 560 14 58</p>
             </div>
         </div>
         <div id="media-container">
-            <a href="https://www.instagram.com" target="_blank"><img style="max-width: 50px;" src="./images/logos/instagram.png" alt="Instagram contact"></a>
-            <a href="http://facebook.com/profile.php?=73322363" target="_blank"><img style="max-width: 50px;" src="./images/logos/Facebook_logo.png" alt="Facebook"></a>
-            <a href="https://www.youtube.com/@TheTehnigga" target="_blank"><img style="max-width: 50px;" src="./images/logos/youtube.png" alt="Youtube"></a>
-            <a href="https://www.imdb.com/name/nm7644480/" target="_blank"><img style="max-width: 50px;" src="./images/logos/imdb.png" alt="IMD"></a>
+            <a style="margin-right: 10px;" href="https://www.instagram.com" target="_blank"><img style="max-width: 50px;" src="./images/logos/instagram.png" alt="Instagram contact"></a>
+            <a style="margin-right: 10px;" href="http://facebook.com/profile.php?=73322363" target="_blank"><img style="max-width: 50px;" src="./images/logos/Facebook_logo.png" alt="Facebook"></a>
+            <a style="margin-right: 10px;" href="https://www.youtube.com/@TheTehnigga" target="_blank"><img style="max-width: 50px;" src="./images/logos/youtube.png" alt="Youtube"></a>
+            <a style="margin-right: 10px;" href="https://www.imdb.com/name/nm7644480/" target="_blank"><img style="max-width: 50px;" src="./images/logos/imdb.png" alt="IMD"></a>
         </div>
     </div>
 
