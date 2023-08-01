a4da1ffa70efc03e0d3ee4b816b3366edce25fa8
carlobettelini <43403045+carlobettelini@users.noreply.github.com>
2023-08-01 10:54:07 +0200

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,25 @@
+document.write(`
+
+<header>
+    <h1>CARLO BETTELINI PORTFOLIO</h1> 
+    <h5 style = "text-align: center; margin-bottom: 10px;"> Videomaker | Game Developer | Junior Software Developer</h5> 
+</header>
+<div id="central">
+        <div id="navigation">
+            <nav>
+                <div class="page-link">
+                    <a href="./index.html">Homepage</a>
+                </div>
+                <div class="page-link">
+                    <a href="./about.html">About me</a>
+                </div>
+                <div class="page-link">
+                    <a href="./videomaker.html">Videomaker</a>
+                </div>
+                <div class="page-link">
+                <a href="./programs.html">Programs</a>
+            </div>
+            </nav>
+        </div>
+
+`);
