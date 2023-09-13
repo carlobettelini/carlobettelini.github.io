e947864e3c862c89f74862959261378c0d6dd9c3
cabit <bettec@usi.ch>
2023-09-14 00:20:21 +0200

    Big change in the front-end and back-end
    Removed the collapsible button, re-designed the site, changed the colors, renamed some pages.

--- js/footer.js
+++ js/footer.js
@@ -1,21 +1,20 @@
 document.write(`
     <div id="footer">
-        <p style="padding: 10px;">© Carlo Bettelini, last page update 5 March 2023</p>
+        <p style="color: white; padding: 10px;">© Carlo Bettelini, last page update 13 September 2023</p>
         <div id="infos">
             <a href="mailto:"carlo.bettelini@gmail.com">
                 <div class="info">
                     <img src="./images/logos/gmail.png " class="images-info">
-                    <p style="padding: 10px;">carlo.bettelini@gmail.com</p>
+                    <p style="color: white; padding: 10px;">carlo.bettelini@gmail.com</p>
                 </div>
             </a>
-            <div class="info"><img src="./images/logos/phone.png" class="images-info">
-                <p style="padding: 10px;">+41 (0) 79 560 14 58</p>
+            <div class="info"><img src="./images/logos/phone-white.png" class="images-info">
+                <p style="color: white; padding: 10px;">+41 (0) 79 560 14 58</p>
             </div>
         </div>
         <div id="media-container">
-            <a style="margin-right: 10px;" href="https://www.instagram.com" target="_blank"><img style="max-width: 50px;" src="./images/logos/instagram.png" alt="Instagram contact"></a>
-            <a style="margin-right: 10px;" href="http://facebook.com/profile.php?=73322363" target="_blank"><img style="max-width: 50px;" src="./images/logos/Facebook_logo.png" alt="Facebook"></a>
-            <a style="margin-right: 10px;" href="https://www.youtube.com/@TheTehnigga" target="_blank"><img style="max-width: 50px;" src="./images/logos/youtube.png" alt="Youtube"></a>
+            <a style="margin-right: 10px;" href="https://github.com/carlobettelini" target="_blank"><img style="max-width: 50px;" src="./images/logos/github.png" alt="Github profile"></a>
+            <a style="margin-right: 10px;" href="https://www.linkedin.com/in/carlo-bettelini/" target="_blank"><img style="max-width: 50px;" src="./images/logos/linkedin.png" alt="Linkedin profile"></a>
             <a style="margin-right: 10px;" href="https://www.imdb.com/name/nm7644480/" target="_blank"><img style="max-width: 50px;" src="./images/logos/imdb.png" alt="IMD"></a>
         </div>
     </div>
