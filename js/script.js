2d25648dbb43f2c994f3477ba4fb4dd5e4482b87
cabit <bettec@usi.ch>
2023-11-07 19:37:37 +0100

    November update 2023
    I changed the videos with teasers of my shorts, added a info tab for the long movies with collapsible infos, and improved mobile phone view

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ for (var i = 0; i < coll.length; i++) {
 
 function w3_open() {
     var width = document.body.clientWidth;
-    if (width > 800) {
+    if (width > 1000) {
         document.getElementById("navigation").style.width = "25%";
     }
     // document.getElementById("main-content").style.marginLeft = "25%";
@@ -18,4 +18,19 @@ function w3_close() {
     document.getElementById("main-content").style.marginLeft = "0%";
     document.getElementById("navigation").style.display = "none";
     document.getElementById("openNav").style.display = "inline-block";
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
